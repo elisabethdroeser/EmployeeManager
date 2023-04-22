4771e07e1390df781b3a092fb5336bc45505e1f5
elisabeth.droeser@gmail.com <elisabeth.droeser@gmail.com>
2023-04-22 16:48:45 +0200

    Add dbcontext file with entitity framework and sql server nuget packages.

---
+++ EmployeeManager/Data/EmployeeManagerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeManager.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Data
+{
+    public class EmployeeManagerDbContext : DbContext; 
+    {
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<Department> Departments => Set<Department>();
+
+    }
+}
